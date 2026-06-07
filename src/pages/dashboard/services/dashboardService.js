68219e198d990b58117cb2fefc11616c51bcccdc
TSwaroop <swaroopthippapathi@gmail.com>
2026-06-07 18:50:04 +0530

    Update BASE_URL to use environment variable

--- src/pages/dashboard/services/dashboardService.js
+++ src/pages/dashboard/services/dashboardService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/dashboard";
-
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/dashboard`;
 /* =========================
    DASHBOARD STATS
 ========================= */
